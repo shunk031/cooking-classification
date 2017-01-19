a9d50b26cdb0d2b47f6dc8a7165aa355e7bc31c6
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-20 00:33:14 +0900

    add labeling script

---
+++ src/label.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pandas as pd
+import pickle
+
+DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath("__file__")), "../dataset")
+CROPPED_DIR = os.path.join(DATASET_DIR, "cropped_images")
+
+if __name__ == '__main__':
+
+    train_labeled_data = pd.read_csv(os.path.join(DATASET_DIR, "clf_train_master.tsv"), delimiter="\t")
+    test_labeled_data = pd.read_csv(os.path.join(DATASET_DIR, "clf_test.tsv"), delimiter="\t")
+
+    train_filenames = train_labeled_data["file_name"]
+    train_labels = train_labeled_data["category_id"]
+
+    train_labeled_image_dataset_list = [
+        (os.path.join(CROPPED_DIR, "cropped_" + filename), label) for filename, label in zip(train_filenames, train_labels)
+    ]
+
+    with open("train_labeled_image_dataset_list.pkl", "wb") as wf:
+        pickle.dump(train_labeled_image_dataset_list, wf)
+
+    print("Dump labeled image dataset list.")
