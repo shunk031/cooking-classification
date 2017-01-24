e03b1ccbe1fd54f26b2e457718eb6694eb393156
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-24 13:37:21 +0900

    add AlexNet Like mode

--- src/model.py
+++ src/model.py
@@ -38,3 +38,39 @@ class AlexNet(chainer.Chain):
         loss = F.softmax_cross_entropy(h, t)
         chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
         return loss
+
+
+class AlexLikeNet(chainer.Chain):
+
+    insize = 227
+
+    def __init__(self):
+        super(AlexLikeNet, self).__init__(
+            conv1=L.Convolution2D(None,  96, 11, stride=4),
+            bn1=L.BatchNormalization(96),
+            conv2=L.Convolution2D(None, 256,  5, pad=2),
+            bn2=L.BatchNormalization(256),
+            conv3=L.Convolution2D(None, 384,  3, pad=1),
+            conv4=L.Convolution2D(None, 384,  3, pad=1),
+            conv5=L.Convolution2D(None, 256,  3, pad=1),
+            fc6=L.Linear(None, 4096),
+            fc7=L.Linear(None, 4096),
+            fc8=L.Linear(None, 25),
+        )
+        self.train = True
+
+    def __call__(self, x, t):
+        h = F.max_pooling_2d(F.local_response_normalization(
+            F.relu(self.bn1(self.conv1(x)))), 3, stride=2)
+        h = F.max_pooling_2d(F.local_response_normalization(
+            F.relu(self.bn2(self.conv2(h)))), 3, stride=2)
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.conv4(h))
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.dropout(F.relu(self.fc6(h)), train=self.train)
+        h = F.dropout(F.relu(self.fc7(h)), train=self.train)
+        h = self.fc8(h)
+
+        loss = F.softmax_cross_entropy(h, t)
+        chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
+        return loss
