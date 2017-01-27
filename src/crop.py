3a35fa244f9006516d423ac4b9e91167b84fe148
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-27 15:22:51 +0900

    add crop_image function

--- src/crop.py
+++ src/crop.py
@@ -8,6 +8,31 @@ import pickle
 DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath("__file__")), "../dataset")
 CROPPED_DIR = os.path.join(DATASET_DIR, "cropped_images")
 
+
+def crop_image(img_path):
+
+    img = Image.open(img_path, "r")
+    square_size = min(img.size)
+    width, height = img.size
+
+    if width > height:
+        top = 0
+        bottom = square_size
+        left = (width - square_size) / 2
+        right = left + square_size
+        box = (left, top, right, bottom)
+    else:
+        left = 0
+        right = square_size
+        top = (height - square_size) / 2
+        bottom = top + square_size
+        box = (left, top, right, bottom)
+
+    crop_img = img.crop(box).resize((256, 256), Image.ANTIALIAS)
+
+    return crop_img
+
+
 if not os.path.isdir(CROPPED_DIR):
     print("Create {}".format(CROPPED_DIR))
     os.makedirs(CROPPED_DIR)
@@ -26,28 +51,8 @@ if __name__ == '__main__':
 
     for i, img_path in enumerate(image_paths):
 
-        img = Image.open(img_path[0], "r")
-
-        print("{:5}/{:5} Now process: {}".format(i, num_of_images, img_path[1]))
-
-        square_size = min(img.size)
-        width, height = img.size
-
-        if width > height:
-            top = 0
-            bottom = square_size
-            left = (width - square_size) / 2
-            right = left + square_size
-            box = (left, top, right, bottom)
-        else:
-            left = 0
-            right = square_size
-            top = (height - square_size) / 2
-            bottom = top + square_size
-            box = (left, top, right, bottom)
-
-        img = img.crop(box)
-        img = img.resize((256, 256), Image.ANTIALIAS)
+        print("{:5}/{:5} Now process: {}".format(i + 1, num_of_images, img_path[1]))
+        crop_img = crop_image(img_path[0])
 
         cropped_image_path = os.path.join(CROPPED_DIR, "cropped_" + img_path[1])
-        img.save(cropped_image_path, "JPEG")
+        crop_img.save(cropped_image_path, "JPEG")
