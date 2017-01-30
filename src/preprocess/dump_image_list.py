3a2bfb11ce3f4727f8c2c1ab40d204885cb8ba2b
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-31 02:04:00 +0900

    support cae image dataset list

--- src/preprocess/dump_image_list.py
+++ src/preprocess/dump_image_list.py
@@ -5,24 +5,38 @@ import argparse
 import pandas as pd
 import pickle
 
-DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath("__file__")), "../dataset")
+DATASET_DIR = os.path.join(os.path.dirname(os.path.abspath("__file__")), "../../dataset")
 CROPPED_DIR = os.path.join(DATASET_DIR, "cropped_images")
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='dump labeled image dataset list')
+    parser.add_argument('--cae', action='store_true')
+    parser.set_defaults(cae=False)
+    args = parser.parse_args()
 
+    # load train file contain image filename and category id
     train_labeled_data = pd.read_csv(os.path.join(DATASET_DIR, "clf_train_master.tsv"), delimiter="\t")
-    test_labeled_data = pd.read_csv(os.path.join(DATASET_DIR, "clf_test.tsv"), delimiter="\t")
 
     train_filenames = train_labeled_data["file_name"]
     train_labels = train_labeled_data["category_id"]
 
-    train_labeled_image_dataset_list = [
-        ("cropped_" + filename, label) for filename, label in zip(train_filenames, train_labels)
-    ]
+    if args.cae:
+        CROPPED_IMAGE_DIR = os.path.join(CROPPED_DIR, 'train_cae')
 
-    with open("train_labeled_image_dataset_list.pkl", "wb") as wf:
-        pickle.dump(train_labeled_image_dataset_list, wf)
+        # get all unlabeled image filenames
+        image_dataset_list = os.listdir(CROPPED_IMAGE_DIR)
+        dump_pkl_filename = "cae_image_dataset_list.pkl"
 
-    print("Dump labeled image dataset list.")
+    else:
+        # get all tuples of filename and category id
+        image_dataset_list = [
+            ("cropped_" + filename, label) for filename, label in zip(train_filenames, train_labels)
+        ]
+        dump_pkl_filename = "labeled_image_dataset_list.pkl"
+
+    # dump image dataset list as pikle file
+    with open(dump_pkl_filename, "wb") as wf:
+        pickle.dump(image_dataset_list, wf)
+
+    print("Dump image dataset list.")
