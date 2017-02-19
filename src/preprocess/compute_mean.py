d9d1e6aac8e1ecd4a2aac959ca6ad1cf41c9427f
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-02-20 03:14:25 +0900

    update compute mean script

--- src/preprocess/compute_mean.py
+++ src/preprocess/compute_mean.py
@@ -3,37 +3,52 @@
 import argparse
 import os
 import sys
-import numpy as np
-import chainer
-from PIL import Image
 import pickle
 
+import numpy as np
 
-def compute_mean(dataset):
+import chainer
+
+from PIL import Image
+from tqdm import tqdm
+from preprocess_dataset import PREPROCESS_TYPES
+
+
+def compute_mean_l(dataset):
 
     sum_image = 0
     N = len(dataset)
 
-    for i, (image, _) in enumerate(dataset):
+    for image, _ in tqdm(dataset):
         try:
             sum_image += image
-            sys.stderr.write("{}/{}\r".format(i + 1, N))
-            sys.stderr.flush()
-        except ValueError as err:
-            print(err)
+        except ValueError:
+            pass
 
-    sys.stderr.write("\n")
+    return sum_image / N
+
+
+def compute_mean_i(dataset):
+
+    sum_image = 0
+    N = len(dataset)
+
+    for image in tqdm(dataset):
+        try:
+            sum_image += image
+        except ValueError:
+            pass
+
     return sum_image / N
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Compute images mean array')
     parser.add_argument('dataset', help='Path to training image-label list file')
+    parser.add_argument('preprocess_type', choices=PREPROCESS_TYPES)
     parser.add_argument('--root', '-R', default='.', help='Root directory path of image files')
     parser.add_argument('--output_path', '-o', default='.', help='path to output mean array')
-    parser.add_argument('--cae', action='store_true')
     parser.add_argument('--image', '-i', action='store_true')
-    parser.set_defaults(cae=False)
     parser.set_defaults(image=False)
     args = parser.parse_args()
 
@@ -41,15 +56,23 @@ if __name__ == '__main__':
     with open(args.dataset, "rb") as rf:
         image_dataset_list = pickle.load(rf)
 
-    print("[ LOAD ] Load dataset images into LabeledImageDataset.")
-    dataset = chainer.datasets.LabeledImageDataset(image_dataset_list, args.root)
-    print("[ CALC ] Compute mean image.")
-    mean = compute_mean(dataset)
+    print("[ LOAD ] Load dataset images.")
+    if args.preprocess_type == "train":
+        dataset = chainer.datasets.LabeledImageDataset(image_dataset_list, args.root)
+        mean_filename = "train_mean"
 
-    if args.cae:
+    elif args.preprocess_type == "cae":
+        dataset = chainer.datasets.ImageDataset(image_dataset_list, args.root)
         mean_filename = "cae_mean"
     else:
-        mean_filename = "train_mean"
+        print("In the test phase, the image-label list is not necessary.")
+        sys.exit()
+
+    print("[ CALC ] Compute mean image.")
+    if args.preprocess_type == "train":
+        mean = compute_mean_l(dataset)
+    else:
+        mean = compute_mean_i(dataset)
 
     output_path = os.path.join(args.output_path, mean_filename)
 
